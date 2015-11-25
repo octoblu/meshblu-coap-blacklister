33efd5f7975ed2b055909aa231d21b53e67a3aae
sqrtofsaturn <sqrt@octoblu.com>
2015-11-25 12:42:46 -0700

    check all services

--- run.sh
+++ run.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
 
+TIMEOUT=${TIMEOUT_SECS:-30}
 CMD_TIMEOUT=${CMD_TIMEOUT_SECS:-5}
+BLACKLIST_TIMEOUT=${BLACKLIST_TIMEOUT_SECS:-120}
 
-while true; do
+healthcheck() {
+  local blacklist_timeout
+  blacklist_timeout=0
+  SERVER=$1
+  SERVER_NUMBER=$(echo ${SERVER} | awk '{ print $1 }')
+  SERVER_IP=$(echo ${SERVER} | awk '{ print $3 }')
+  SERVER_PORT=$(echo ${SERVER} | awk '{ print $5 }')
 
-  timeout $CMD_TIMEOUT coap -q coap://${COAP_HOST}/status 2>&1 > /dev/null
-
+  timeout $CMD_TIMEOUT coap -q coap://${SERVER_IP}:${SERVER_PORT}/status 2>&1 > /dev/null
   if [ $? -ne 0 ]; then
-    SERVER=$(penctl ${PENCTL_HOST} servers | grep $HOST_IP | awk '{ print $1 }')
-    penctl ${PENCTL_HOST} server ${SERVER} blacklist 60
+    blacklist_timeout=${BLACKLIST_TIMEOUT}
   fi
+  penctl ${PENCTL_HOST} server ${SERVER_NUMBER} blacklist $blacklist_timeout
+}
 
-  sleep ${TIMEOUT_SECS:-30}
-done
+main() {
+  while true; do
+    local servers
+    SERVERS_RAW=$(penctl ${PENCTL_HOST} servers)
+    IFS=$'\n' read -rd '' -a servers <<<"$SERVERS_RAW"
+
+    for server in "${servers[@]}"; do
+      healthcheck "${server}"
+    done
+
+    sleep ${TIMEOUT}
+  done
+}
+main
